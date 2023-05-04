eaa75e9fe49d5b6e8868e9657bf1683f9ca19247
Akshay Kalambe <a.kalambe@interproductec.com>
2023-05-04 10:43:20 +0530

    Added basic files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3003',
+  credentials: true,
+  exposedHeaders: ['Set-Cookie'],
+};
+
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+
+app.post('/', (req, res) => {
+  res.cookie('mycookie', 'cookie value', {
+    maxAge: 86400000,
+    httpOnly: true,
+    // sameSite: 'none',
+    // domain: 'http://localhost:3003',
+    path: '/',
+  });
+  res.json('Cookie set!');
+});
+
+app.listen(3030, () => console.log('Server started on port 3030'));
